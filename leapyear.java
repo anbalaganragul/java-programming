265aa627ae0937683a7cda061d98fc38bcf62a91
anbalaganragul <43032333+anbalaganragul@users.noreply.github.com>
2018-10-03 12:14:49 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,16 @@
+
+		class Main {
+  public static void main(String[] args) {
+       int leapYear = 1900; 
+
+        while (leapYear <= 2020) { 
+            for (int x = 0; x < 10; x++) {
+                System.out.print(leapYear + " "); 
+                leapYear = leapYear + 4;
+            }
+            System.out.println();
+    }
+}
+}
+	}
+}
