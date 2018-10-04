3200216504bc91eb7025e7bd739368ece0c4e31c
anbalaganragul <43032333+anbalaganragul@users.noreply.github.com>
2018-10-04 12:05:03 +0530

    Create largest number.java

---
+++ largest number.java
@@ -0,0 +1,14 @@
+public class Largest 
+{
+
+ public static void main(String[] args) {
+
+        double n1 = 10, n2 = 20, n3 = 50;
+        if( n1 >= n2 && n1 >= n3)
+        System.out.println(n1 + " is the largest number.");
+        else if (n2 >= n1 && n2 >= n3)
+        System.out.println(n2 + " is the largest number.");
+        else
+        System.out.println(n3 + " is the largest number.");
+    }
+}
