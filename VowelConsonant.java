813d44099c8a56ed15b6342cc53467e74970e4f5
anbalaganragul <43032333+anbalaganragul@users.noreply.github.com>
2018-10-05 12:42:40 +0530

    Update VowelConsonant.java

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -1,7 +1,11 @@
-public class VowelConsonant
+import java.util.*;
+import java.util.Scanner;
+public class Main
 {
     public static void main(String[] args) {
-        char ch = 'i';
+        char ch ;
+        Scanner sc=new Scanner(System.in);
+        ch = sc.next().charAt(0);
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
         else
